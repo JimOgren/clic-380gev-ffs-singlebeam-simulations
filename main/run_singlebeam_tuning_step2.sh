b02f6f575afe84afa3e3cd1ca4f667ea07ae6404
jogren <jim.ogren@cern.ch>
2020-01-23 12:19:35 +0100

    Uncommented source command in run files for Condor.

--- main/run_singlebeam_tuning_step2.sh
+++ main/run_singlebeam_tuning_step2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # If running on lxplus or HTCondor, uncomment to setup placet etc.:
-# source /cvmfs/clicbp.cern.ch/x86_64-slc6-gcc62-opt/setup.sh
+source /cvmfs/clicbp.cern.ch/x86_64-slc6-gcc62-opt/setup.sh
 
 mkdir -p temp_run
 cd temp_run
