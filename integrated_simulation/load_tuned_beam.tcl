873d8f9d0d3205fc665f8a7c2717e67ec31681c5
jogren <jim.ogren@cern.ch>
2020-01-28 14:51:20 +0100

    Fixed load tuned beam.

--- integrated_simulation/load_tuned_beam.tcl
+++ integrated_simulation/load_tuned_beam.tcl
@@ -1,9 +1,9 @@
 # Load beam
 Octave {
-   disp("\nLoading beam from integrated simulation - UPDATED2")
+   disp("\nLoading beam from integrated simulation")
 
    disp('Loading beam${beam_case}')
-   load /afs/cern.ch/work/j/jogren/TuningStudies/CLIC_FFS_380GeV/tuned_beams_updated2/beam-${beam_case}.dat;
+   load $script_dir/beams/tuned_beams/beam-${beam_case}.dat;
    BeamIn = beam_${beam_case};
    placet_set_beam("beam0t", BeamIn);
 }
