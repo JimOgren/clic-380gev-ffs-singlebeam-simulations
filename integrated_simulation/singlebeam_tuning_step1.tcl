e9964decd835994533fb02d7937b82e8bcb88258
jogren <jim.ogren@cern.ch>
2020-01-28 15:08:47 +0100

    integrated simulation tuning step 1 runs successfully locally.

--- integrated_simulation/singlebeam_tuning_step1.tcl
+++ integrated_simulation/singlebeam_tuning_step1.tcl
@@ -2,19 +2,46 @@
 #
 # Tuning studies for single beam
 #
+# Step 1: beam-based alignment and tuning
+# Loads a beam from an integrated simulation study where RTML, ML and BDS
+# have been corrected. In this script we take this beam as input and tune
+# the final-focus system. The beams were provided by C. Gohil. In the original
+# version 100 beam cases were tested. In this repo we put one case.
+#
+# Written by Jim Ogren, CERN, 2018-2020
+#
 #############################################################################
 set t_1 [clock seconds]
+global guinea_exec
+set run_on_lxplus 0
+
+if { $run_on_lxplus } {
+   set script_dir /afs/cern.ch/work/j/jogren/TuningStudies/CLIC_FFS_380GeV/Clean_scripts/clic-380gev-ffs-singlebeam-simulations
+   set guinea_exec /afs/cern.ch/eng/sl/clic-code/lx64slc5/guinea-pig/bin/guinea-old
+} else {
+   set script_dir /home/jim/GIT/clic-380gev-ffs-singlebeam-simulations
+   set guinea_exec /home/jim/bin/guinea
+}
+
+set res_dir $script_dir/integrated_simulation/Results
+set save_dir $res_dir/Results_step1
+
+# Check if folders exist
+if {![file exist $script_dir]} {
+   puts "script_dir path does not exist!"
+   exit
+}
+
+if {![file exist $res_dir]} {
+   exec bash -c "mkdir -p $res_dir"
+}
+if {![file exist $save_dir]} {
+   exec bash -c "mkdir -p $save_dir"
+}
 
 set e_initial 190
 set e0 $e_initial
-
-set script_dir /afs/cern.ch/work/j/jogren/TuningStudies/CLIC_FFS_380GeV/SingleBeamTuning_v07_tuned_beams
-
-if {![file exist $script_dir]} {
-    puts "script_dir path does not exist"
-    set script_dir [pwd]/..
-}
-
+#############################################################################
 # command-line options
 # sr          : synchrotron radiation on/off
 # sigma       : misalignment in um
@@ -29,23 +56,23 @@ if {![file exist $script_dir]} {
 # wdisp       : gain for dispersion target steering (dts)
 # iterdts     : number of dts iterations
 # gaindts     : gain for dts
-# beam_case   : which beam to load: 8, 20, 30 (vertical emittance)
+# beam_case   : which beam to load: 0-99, beams from integrated simulations
 
 array set args {
-    sr 1
-    sigma 10.0
-    sigmaM 10.0
-    sigmak 1e-4
-    sigmaroll 100.0
-    bpmres 0.020
-    deltae 0.001
-    machine 1
-    loadmachine 0
-    measure_response 0
-    wdisp 0.71
-    iterdts 30
-    gaindts 0.5
-    beam_case 0
+   sr 1
+   sigma 10.0
+   sigmaM 10.0
+   sigmak 1e-4
+   sigmaroll 100.0
+   bpmres 0.020
+   deltae 0.001
+   machine 1
+   loadmachine 0
+   measure_response 0
+   wdisp 0.71
+   iterdts 30
+   gaindts 0.5
+   beam_case 0
 }
 
 array set args $argv
@@ -64,8 +91,8 @@ set iterdts $args(iterdts)
 set gaindts $args(gaindts)
 set beam_case $args(beam_case)
 
-set machine $beam_case
-puts "Machine $machine and beam_case $beam_case UPDATED2"
+
+puts "Beam_case $beam_case"
 
 #############################################################################
 # load header files:
@@ -73,31 +100,30 @@ source $script_dir/scripts/make_beam.tcl
 source $script_dir/scripts/wake_calc.tcl
 source $script_dir/scripts/octave_functions.tcl
 source $script_dir/main/beam_parameters.tcl
-
-# load lattice and create beams
-source $script_dir/main/loadlattice.tcl
-source $script_dir/main/createbeams.tcl
 source $script_dir/scripts/calc_lumi.tcl
 
-# Load beams from integrated simulation, tuned beams
-source $script_dir/scripts/load_beam_updated2.tcl
+# load lattice and create beams
+source $script_dir/scripts/load_lattice.tcl
+source $script_dir/scripts/create_beams.tcl
+
+# Load beams from integrated simulation
+source $script_dir/integrated_simulation/load_tuned_beam.tcl
 
 FirstOrder 1
 
 # Go through lattice file and find indices for different elements:
 source $script_dir/scripts/element_indices.tcl
 
-# Measure target dispersion
-if { $measure_response } {
-   source $script_dir/scripts/measure_nominal_dispersion.tcl
-}
+# load target dispersion
+source $script_dir/scripts/load_machine_model_single_particle.tcl
 
-# load target dispersion 
-source $script_dir/scripts/load_machine_model.tcl
+# Load BBA methods
+source $script_dir/scripts/beam_based_alignment.tcl
 
-# load knobs
-source $script_dir/main/setup_knobs.tcl
+# Load tuning methods
+source $script_dir/scripts/setup_knobs.tcl
 
+#############################################################################
 # Set same offsets and angles to single particle beams
 Octave {
 	Bt = placet_get_beam("beam0t");
@@ -110,7 +136,7 @@ Octave {
 	B0(2:6) = Bmean(2:6);
 	B1(2:6) = Bmean(2:6);
 	B2(2:6) = Bmean(2:6);
-	
+
 	placet_set_beam("beam0", B0);
 	placet_set_beam("beam1", B1);
 	placet_set_beam("beam2", B2);
@@ -119,18 +145,17 @@ Octave {
 Octave {
 	placet_element_set_attribute("test", BI, "resolution", $bpmres);
 }
-source $script_dir/scripts/checkStatus.tcl
 
 
-# Step 1 misalign the machine
+# Procedure for misaligning the machine
 #############################################################################
 # Procedure for misaligning a machine
 proc my_survey { beamline_name } {
-   global machine sigma sigmaM sigmaroll sigmak bpmres
+   global beam_case sigma sigmaM sigmaroll sigmak bpmres
    puts " "
    puts "Random misalignment errors"
    Octave {
-      randn("seed", ($machine+1) * 234);
+      randn("seed", ($beam_case+1) * 234);
       placet_element_set_attribute("$beamline_name", DI, "strength_x", 0.0);
       placet_element_set_attribute("$beamline_name", DI, "strength_y", 0.0);
       placet_element_set_attribute("$beamline_name", BI, "resolution", $bpmres);
@@ -155,22 +180,26 @@ proc my_survey { beamline_name } {
    }
 }
 
-# Misalign the machine
+# Start simulation
+#############################################################################
+# Step 0: check status of perfect machine
+source $script_dir/scripts/check_status_single_particle.tcl
+
+# Misalign the machine and check status
 my_survey "test"
+source $script_dir/scripts/check_status_single_particle.tcl
 
-source $script_dir/scripts/checkStatus.tcl
-
-# BBA
-##########################################################
-source $script_dir/main/beam_based_alignment.tcl
-source $script_dir/scripts/checkStatus.tcl
+# Run BBA
+#############################################################################
+run_beam_based_alignment
+source $script_dir/scripts/check_status_single_particle.tcl
 
 # Align and tune sextupoles (transverse position)
 #############################################################################
 align_sextupoles_reverse 10
-source $script_dir/scripts/checkStatus.tcl
+source $script_dir/scripts/check_status_single_particle.tcl
 
+# Save beamline and tuning data
 #############################################################################
-save_beamline_status "test" /afs/cern.ch/work/j/jogren/TuningStudies/CLIC_FFS_380GeV/SingleBeamTuning_v07_tuned_beams/main/Results/Results_with_misalignments_updated2/machine_status_step1_$beam_case.dat
-save_tuning_data /afs/cern.ch/work/j/jogren/TuningStudies/CLIC_FFS_380GeV/SingleBeamTuning_v07_tuned_beams/main/Results/Results_with_misalignments_updated2/tuning_data_step1_$beam_case.dat
-
+save_beamline_status "test" $save_dir/machine_status_$machine.dat
+save_tuning_data $save_dir/tuning_data_$machine.dat $t_1
